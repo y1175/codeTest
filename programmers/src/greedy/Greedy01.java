f365edd404e1974b22a14ec71c9f19fe8fbdc9ca
y1175 <lordofbabo@gmail.com>
2020-09-18 21:58:08 +0900

    dd

---
+++ programmers/src/greedy/Greedy01.java
@@ -0,0 +1,41 @@
+package greedy;
+
+public class Greedy01 {
+	  public int solution(int n, int[] lost, int[] reserve) {
+
+	      int  answer=0;
+	         answer= n-lost.length;
+	        
+	                for(int i=0; i< lost.length; i++) {
+	            for(int j=0; j< reserve.length; j++) {
+
+	                if(lost[i] == reserve[j]) {
+	                    lost[i] = -1;
+	                    reserve[j] = -1;
+	                   answer++;
+	                    break;
+	                }
+	            }
+	        }
+	        
+	        
+	     for(int i=0; i<lost.length;i++)
+	     {
+	    	 for(int q=0; q<reserve.length;q++)
+	    	 {          
+	    if(lost[i]-reserve[q]==1 || lost[i]-reserve[q]==-1)
+	    	 {
+	             reserve[q]=-1;
+	    		answer++;
+	    		 
+	    		 break;
+	    	 }
+	            
+	    	 }
+	     }
+	    	
+	    	
+	        
+	        return answer;
+	    }
+}
